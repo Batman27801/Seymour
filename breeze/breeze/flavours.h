69f7179a21aa29cfdae1c5437555030682c1038b
batman27801 <k191449@nu.edu.pk>
2020-05-06 21:30:56 +0500

    added every releveant header file into headers.h

--- breeze/breeze/flavours.h
+++ breeze/breeze/flavours.h
@@ -4,9 +4,20 @@ class flavour
 protected:
     double price;
     char* description;
+    char* name;
 public:
+    flavour(char* flavname)
+    {
+        char* name = new char[230];
+        name = flavname;
+    }
     double virtual get_price() = 0;
-     double virtual set_price() = 0;
-     virtual char*  get_description() = 0;
+    double virtual set_price() = 0;
+    virtual char*  get_description() = 0;
     virtual char* set_description() = 0;
+    char* get_name()
+    {
+        return name;
+    }
 };
+
